b9a2bc855bbd7a7d54c9e280ee875393e30cf1a6
christianchiarulli <chrisatmachine@gmail.com>
2021-12-14 17:59:55 -0500

    add autopairs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,6 +44,7 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -7,6 +7,9 @@ configs.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
+  autopairs = {
+		enable = true,
+	},
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "" }, -- list of language that will be disabled

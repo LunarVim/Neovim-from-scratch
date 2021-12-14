94f160d1bda6c8a62caad92a1abd2c2582f2c9be
christianchiarulli <chrisatmachine@gmail.com>
2021-12-14 17:52:10 -0500

    add telescope

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -66,6 +66,9 @@ return packer.startup(function(use)
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
 
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

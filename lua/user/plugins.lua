3ae7d69154d202dd303405f7eff4d360eb107ee3
christianchiarulli <chrisatmachine@gmail.com>
2021-12-14 18:33:51 -0500

    add lualine

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -50,6 +50,7 @@ return packer.startup(function(use)
   use "kyazdani42/nvim-tree.lua"
   use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
+  use 'nvim-lualine/lualine.nvim'
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out

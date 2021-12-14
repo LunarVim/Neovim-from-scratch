03ec92cafb62f4331bd04bee734d0bd2c28e668e
christianchiarulli <chrisatmachine@gmail.com>
2021-12-14 18:18:28 -0500

    add nvimtree
    update

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -46,6 +46,8 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
   use "numToStr/Comment.nvim" -- Easily comment stuff
+  use 'kyazdani42/nvim-web-devicons'
+  use 'kyazdani42/nvim-tree.lua'
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out

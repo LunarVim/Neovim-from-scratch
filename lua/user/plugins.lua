0b689f091596b1f6a06e74d40b379a722953d919
christianchiarulli <chrisatmachine@gmail.com>
2021-12-14 18:25:42 -0500

    add null-ls

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -71,6 +71,7 @@ return packer.startup(function(use)
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
+  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"

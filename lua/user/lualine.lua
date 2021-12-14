91e22c4076ae58cec23ac5a9b2ea7181dd8f988a
christianchiarulli <chrisatmachine@gmail.com>
2021-12-14 18:58:04 -0500

    add whichkey
    add whickey
    
    fix
    
    t
    
    fix close

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -68,7 +68,7 @@ lualine.setup({
 		theme = "auto",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
+		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
 		always_divide_middle = true,
 	},
 	sections = {

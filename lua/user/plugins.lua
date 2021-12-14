2683495c3df5ee7d3682897e0d47b0facb3cedc9
christianchiarulli <chrisatmachine@gmail.com>
2021-12-14 17:08:44 -0500

    add completion
    add completion

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -47,7 +47,18 @@ return packer.startup(function(use)
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim" -- A bunch of colorschemes you can try out
+  use "lunarvim/darkplus.nvim"
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins

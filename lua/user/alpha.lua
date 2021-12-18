6013f44c0aeb17df3b5d032047e75c0e92e9eb21
Francisco De La Cruz <helloquico@gmail.com>
2021-12-18 16:06:45 -0500

    setup fortune

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -24,11 +24,10 @@ dashboard.section.buttons.val = {
 
 local function footer()
 -- NOTE: requires the fortune-mod package to work
-	-- local handle = io.popen("fortune")
-	-- local fortune = handle:read("*a")
-	-- handle:close()
-	-- return fortune
-	return "chrisatmachine.com"
+	local handle = io.popen("fortune")
+	local fortune = handle:read("*a")
+	handle:close()
+	return fortune
 end
 
 dashboard.section.footer.val = footer()

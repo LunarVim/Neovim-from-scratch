323d6fb1e4dc3f1823fd8f0c831f62961ee67e81
Francisco De La Cruz <helloquico@gmail.com>
2021-12-19 18:59:07 -0500

    turn on lsp virtual text

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -15,7 +15,7 @@ M.setup = function()
 
   local config = {
     -- disable virtual text
-    virtual_text = false,
+    virtual_text = true,
     -- show signs
     signs = {
       active = signs,

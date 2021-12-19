3c1a99a1bac88287b0aee3c6da0d1e4ca4a0742c
Francisco De La Cruz <helloquico@gmail.com>
2021-12-19 17:14:26 -0500

    add colorscheme configurations

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -6,3 +6,11 @@ catch /^Vim\%((\a\+)\)\=:E185/
   set background=dark
 endtry
 ]]
+
+vim.g.solarized_italic_comments = true
+vim.g.solarized_italic_keywords = true
+vim.g.solarized_italic_functions = true
+vim.g.solarized_italic_variables = false
+vim.g.solarized_contrast = true
+vim.g.solarized_borders = false
+vim.g.solarized_disable_background = false

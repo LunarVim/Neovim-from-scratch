6169d6a10b60226a9fca5272b1acbdae5d2d6239
christianchiarulli <chrisatmachine@gmail.com>
2021-12-28 02:40:26 -0500

    fix commenting with gc and gcc

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -15,8 +15,8 @@ local setup = {
     -- No actual key bindings are created
     presets = {
       operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-      motions = false, -- adds help for motions
-      text_objects = false, -- help for text objects triggered after entering an operator
+      motions = true, -- adds help for motions
+      text_objects = true, -- help for text objects triggered after entering an operator
       windows = true, -- default bindings on <c-w>
       nav = true, -- misc bindings to work with windows
       z = true, -- bindings for folds, spelling and others prefixed with z

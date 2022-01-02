65466d0b009251f5dfc745abfac9540f2b597eb8
christianchiarulli <chrisatmachine@gmail.com>
2022-01-02 14:37:51 -0500

    diagnostic flake8 enabled by default

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -14,6 +14,6 @@ null_ls.setup({
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-    -- diagnostics.flake8
+    diagnostics.flake8
 	},
 })

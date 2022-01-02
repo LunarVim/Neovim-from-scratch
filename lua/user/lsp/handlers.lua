efd287ffa01e611a0981b34e70538528e5e96fdb
christianchiarulli <chrisatmachine@gmail.com>
2022-01-02 14:27:12 -0500

    update deprecated diagnostic show

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -76,7 +76,7 @@ local function lsp_keymaps(bufnr)
     bufnr,
     "n",
     "gl",
-    '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>',
+    '<cmd>lua vim.diagnostic.open_float()<CR>',
     opts
   )
   vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)

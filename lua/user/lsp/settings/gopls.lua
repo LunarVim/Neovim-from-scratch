89c10268bc3a940fc728db1df3e1ff9fe3606efe
David Giacomo <dgiacomo@gmail.com>
2022-02-03 00:29:28 -0800

    seems to work after building

--- lua/user/lsp/settings/gopls.lua
+++ lua/user/lsp/settings/gopls.lua
@@ -1,8 +1,32 @@
 return {
   settings = {
+    cmd_env = {
+      GOROOT="/Users/dgiacomo/go/src/github.com/muxinc/mux/bazel-mux/external/go_sdk",
+      GOPACKAGESDRIVER="/Users/dgiacomo/go/src/github.com/muxinc/mux/tools/bazel/gopackagesdriver.sh"
+    }, 
     gopls = {
+      verboseOutput = true,
+      directoryFilters = {
+          "-bazel-bin",
+          "-bazel-out",
+          "-bazel-testlogs",
+          "-bazel-mux"
+      },
+        gofumpt = true,
+        ["local"] = "github.com/muxinc/mux",
+        usePlaceholders = true,
+        semanticTokens = true,
+        codelenses = {
+          gc_details = false,
+          regenerate_cgo = false,
+          generate = false,
+          test =false,
+          tidy =false,
+          upgrade_dependency = false,
+          vendor = false
+      },
       analyses = {
-      unusedparams = true,
+        unusedparams = true,
     },
       staticcheck = true,
     },

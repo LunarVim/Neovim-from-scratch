eebce333df7a0d458af560f714e78a442c791d14
David Giacomo <dgiacomo@gmail.com>
2022-02-03 18:27:13 -0800

    got vimwiki config working - also changed front page

--- lua/user/vimwiki.lua
+++ lua/user/vimwiki.lua
@@ -1,4 +1,3 @@
-config = function()
 vim.g.vimwiki_list = {
   { 
     path = '~/vimwiki',
@@ -17,9 +16,7 @@ vim.g.indentLine_conceallevel = 2 -- indentline controlls concel
 
 vim.api.nvim_set_keymap("n", "<F3>", ":VimwikiDiaryPrevDay<CR>", { noremap = true, silent = true, nowait = true })
 vim.api.nvim_set_keymap("n", "<F4>", ":VimwikiDiaryNextDay<CR>", { noremap = true, silent = true, nowait = true })
-
-end
-
+vim.api.nvim_set_keymap('!', '<F4>', ':VimwikiDiaryNextDay<CR>', {nowait = true})
 
 vim.cmd [[
  autocmd BufNewFile */diary/????-??-??.md call New_vimwiki_diary_template()
@@ -27,11 +24,7 @@ vim.cmd [[
 function New_vimwiki_diary_template()
 "" If it's Friday, open a end of week lookback template
    if system('date +%u') == 5
-<<<<<<< HEAD
         read ~/vimwiki/Diary\ TemplateEndOfWeek.md | execute "normal ggdd"
-=======
-        read ~/vimwiki/new_vimwiki_diary_templateDiary\ TemplateEndOfWeek.md | execute "normal ggdd"
->>>>>>> de95ae2 (adding vimwiki and lua config for it, fugitive and other git plugins and ripgrep)
     else
         read  ~/vimwiki/Diary\ Template.md | execute "normal ggdd"
     end

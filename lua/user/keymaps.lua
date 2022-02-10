aee3e63a6790d19f7d3b27d9ddd32b2e4c6a7f3c
droctothorpe <mythicalsunlight@gmail.com>
2022-02-09 23:47:55 -0500

    Add missing space in comment

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -5,7 +5,7 @@ local term_opts = { silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
---Remap space as leader key
+-- Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "

0d6b213f318096a3b6b3e8a93cc3261bf376a792
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-03-31 08:07:58 -0700

    Fix comment to make it more clear what the keymap does

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -40,7 +40,7 @@ keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
 -- Insert --
--- Press jk fast to enter
+-- Press jk fast to exit insert mode 
 keymap("i", "jk", "<ESC>", opts)
 
 -- Visual --

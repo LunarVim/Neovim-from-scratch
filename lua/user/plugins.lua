752cea7f90ab02d712c187e9a4969c4da1c6231f
Marius Sotto <chino.sotto@gmail.com>
2022-04-22 21:10:14 +0800

    vimlatex setup and configuration

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -94,6 +94,9 @@ return packer.startup(function(use)
   -- Git
   use "lewis6991/gitsigns.nvim"
 
+  -- vimtex
+  use "lervag/vimtex"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

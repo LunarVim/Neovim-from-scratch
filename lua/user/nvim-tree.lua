86584fd506d4a214dc41e33afa47b903bb4d9740
Chino Sotto <31561010+sottoms@users.noreply.github.com>
2022-04-22 13:16:43 +0800

    Turned off 'auto_close' due to warning
    https://github.com/kyazdani42/nvim-tree.lua 
    
    See tips and reminders section for a script if you'd like to get it to work.

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -42,7 +42,8 @@ nvim_tree.setup {
     "dashboard",
     "alpha",
   },
-  auto_close = true,
+  -- Neovim 0.7 fix
+  auto_close = false,
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,

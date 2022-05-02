22baa416bac29dfe7ed1fcc375a16d65784d9842
micro <micro@gnu-box>
2022-05-02 10:27:00 +0000

    adding a global pcall

--- init.lua
+++ init.lua
@@ -1,21 +1,33 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.gitsigns"
-require "user.nvim-tree"
-require "user.bufferline"
-require "user.lualine"
-require "user.toggleterm"
-require "user.project"
-require "user.impatient"
-require "user.indentline"
-require "user.alpha"
-require "user.whichkey"
-require "user.autocommands"
+
+local core_modules = {
+
+"user/options",
+"user/keymaps",
+"user/plugins",
+"user/colorscheme",
+"user/cmp",
+"user/lsp",
+"user/telescope",
+"user/treesitter",
+"user/autopairs",
+"user/comment",
+"user/gitsigns",
+"user/nvim-tree",
+"user/bufferline",
+"user/lualine",
+"user/toggleterm",
+"user/project",
+"user/impatient",
+"user/indentline",
+"user/alpha",
+"user/whichkey",
+"user/autocommands",
+
+}
+-- Using pcall we can handle better any loading issues
+for _, module in ipairs(core_modules) do
+    local ok, err = pcall(require, module)
+    if not ok then
+      return
+    end
+end

d5baec5bd8878e92d650791c0dd4cd20d0462027
Sun Yi <iuamyy@163.com>
2022-05-07 22:25:16 +0800

    tab size

--- lua/user/basic.lua
+++ lua/user/basic.lua
@@ -16,7 +16,7 @@ vim.wo.colorcolumn = "80"
 -- 缩进2个空格等于一个Tab
 vim.o.tabstop = 4
 vim.bo.tabstop = 4
-vim.o.softtabstop = 2
+vim.o.softtabstop = 4
 vim.o.shiftround = true
 -- >> << 时移动长度
 vim.o.shiftwidth = 2

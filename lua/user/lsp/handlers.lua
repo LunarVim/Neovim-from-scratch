658cc106093c95cdd8bbe770ae1e03a03fa35fa3
Briain O'Driscoll <briainodriscoll@gmail.com>
2022-05-09 19:15:51 +0100

    temporary fix to banish the error

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -46,18 +46,13 @@ end
 
 local function lsp_highlight_document(client)
   -- Set autocommands conditional on server_capabilities
-  if client.server_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]],
-      false
-    )
-  end
+  -- if client.server_capabilities.document_highlight then
+    local status_ok, illuminate = pcall(require, "illuminate")
+    if not status_ok then
+      return
+    end
+    illuminate.on_attach(client)
+  -- end
 end
 
 local function lsp_keymaps(bufnr)
@@ -84,13 +79,15 @@ local function lsp_keymaps(bufnr)
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
-M.on_attach = function(client, bufnr)
-  if client.name == "tsserver" then
-    client.server_capabilities.document_formatting = false
-  end
-  lsp_keymaps(bufnr)
-  lsp_highlight_document(client)
-end
+-- M.on_attach = function(client, bufnr)
+-- vim.notify(client.name .. " starting...")
+-- TODO: refactor this into a method that checks if string in list
+--   if client.name == "tsserver" then
+--     client.resolved_capabilities.document_formatting = false
+--   end
+--   lsp_keymaps(bufnr)
+--   lsp_highlight_document(client)
+-- end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 

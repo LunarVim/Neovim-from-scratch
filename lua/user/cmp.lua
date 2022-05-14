23c927e16df9b4c5ae0ed08899a2e87563d535ea
Rohan Ghige <rohanghige@gmail.com>
2022-05-14 14:28:47 +0530

    [FIX] cmp documentation error

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -119,8 +119,10 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  window = {
+    documentation = {
+      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    },
   },
   experimental = {
     ghost_text = false,

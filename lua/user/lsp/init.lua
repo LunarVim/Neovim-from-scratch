4511d8a7fc0b1cfe2e8cff10c45d66bd6a2d9890
Sun Yi <iuamyy@163.com>
2022-05-14 16:22:13 +0800

    Fix lsp and remove null-ls
    1. null-ls have some iusses with cpp (clangd), so
       remove it.

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -5,4 +5,4 @@ end
 
 require "user.lsp.lsp-installer"
 require("user.lsp.handlers").setup()
-require "user.lsp.null-ls"
+-- require "user.lsp.null-ls"
--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,11 +1,10 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-  vim.notify("没有找到 null-ls")
-  return
+    vim.notify("没有找到 null-ls")
+    return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 
 local formatting = null_ls.builtins.formatting
@@ -13,58 +12,59 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
-  debug = false,
-  sources = {
-    -- Formatting ---------------------
-    --  brew install shfmt
-    formatting.shfmt,
-    -- StyLua
-    formatting.stylua,
-    formatting.clang_format,
+    debug = false,
+    sources = {
+        -- Formatting ---------------------
+        --  brew install shfmt
+        formatting.shfmt,
+        -- StyLua
+        formatting.stylua,
+        --formatting.clang_format,
 
-    -- frontend
-    formatting.prettier.with({ -- 比默认少了 markdown
-      filetypes = {
-        "javascript",
-        "typescript",
-        "css",
-        "scss",
-        "less",
-        "html",
-        "json",
-        "yaml",
-      },
-      prefer_local = "node_modules/.bin",
-    }),
-    -- rustfmt
-    -- rustup component add rustfmt
-    formatting.rustfmt,
-    -- Python
-    -- pip install black
-    -- asdf reshim python
-    formatting.black.with({ extra_args = { "--fast" } }),
-    -----------------------------------------------------
-    -- Ruby
-    -- gem install rubocop
-    -- formatting.rubocop,
-    -----------------------------------------------------
-    -- formatting.fixjson,
-    -- Diagnostics  ---------------------
-    diagnostics.eslint.with({
-      prefer_local = "node_modules/.bin",
-    }),
-    --
-    -- code actions ---------------------
-    code_actions.gitsigns,
-    code_actions.eslint.with({
-      prefer_local = "node_modules/.bin",
-    }),
-  },
-  -- #{m}: message
-  -- #{s}: source name (defaults to null-ls if not specified)
-  -- #{c}: code (if available)
-  diagnostics_format = "[#{s}] #{m}",
-  on_attach = function(_)
-    vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()']])
-  end,
+        -- frontend
+        formatting.prettier.with({ -- 比默认少了 markdown
+            filetypes = {
+                "javascript",
+                "typescript",
+                "css",
+                "scss",
+                "less",
+                "html",
+                "json",
+                "yaml",
+            },
+            prefer_local = "node_modules/.bin",
+        }),
+        -- rustfmt
+        -- rustup component add rustfmt
+        formatting.rustfmt,
+        -- Python
+        -- pip install black
+        -- asdf reshim python
+        formatting.black.with({ extra_args = { "--fast" } }),
+        -----------------------------------------------------
+        -- Ruby
+        -- gem install rubocop
+        -- formatting.rubocop,
+        -----------------------------------------------------
+        -- formatting.fixjson,
+        -- Diagnostics  ---------------------
+        diagnostics.eslint.with({
+            prefer_local = "node_modules/.bin",
+        }),
+        --
+        -- code actions ---------------------
+        code_actions.gitsigns,
+        -- code_actions.clang_format,
+        code_actions.eslint.with({
+            prefer_local = "node_modules/.bin",
+        }),
+    },
+    -- #{m}: message
+    -- #{s}: source name (defaults to null-ls if not specified)
+    -- #{c}: code (if available)
+    diagnostics_format = "[#{s}] #{m}",
+    on_attach = function(_)
+        vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()']])
+    end,
 })

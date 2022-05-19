e83de36a0b9e1bba33238bcf6a31444c6a660051
zigster64 <steve@raffmail.net>
2022-05-19 17:18:12 +1000

    turns out wont need many custom keymaps - default has some excellent menus

--- lua/user/steveo-keymaps.lua
+++ lua/user/steveo-keymaps.lua
@@ -1,6 +1,6 @@
 local opts = { noremap = true, silent = true }
 
-local term_opts = { silent = true }
+-- local term_opts = { silent = true }
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap

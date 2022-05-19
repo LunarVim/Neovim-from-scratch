dda54608b00d795af23e5e46ad6b2b0a79fe53b1
zigster64 <steve@raffmail.net>
2022-05-19 14:14:52 +1000

    Fix some basics still getting fonts sorted

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {

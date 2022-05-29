e02b7d9e2bf7a05cccb1e82a6bc7ca44b223a065
Kevin James Lausen <opensourceconstruction@gmail.com>
2022-05-29 11:16:42 -0400

    Disabled the disabling of the showmode option, b/c I like a highly verbose editor.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -10,7 +10,6 @@ vim.opt.incsearch = true                        -- Turn on incremental search hi
 vim.opt.ignorecase = true                       -- ignore case in search patterns
 vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim
 vim.opt.pumheight = 10                          -- pop up menu height
-vim.opt.showmode = false                        -- we don't need to see things like -- INSERT -- anymore
 vim.opt.showtabline = 2                         -- always show tabs
 vim.opt.smartcase = true                        -- smart case
 vim.opt.smartindent = true                      -- make indenting smarter again

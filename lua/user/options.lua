d192bf548803e3d429f09b34a2c5ea786dc3cb1e
Kevin James Lausen <opensourceconstruction@gmail.com>
2022-05-30 04:04:03 -0400

    Finally restored my special-list-chars.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -40,15 +40,23 @@ local options = {
 -- Did NOT fit well within the options loop above, without throwing errors and disabling the loop.
   vim.opt.shortmess:append "I"                    -- Set No Intro-message for Neo-vim.
 
--- vim.opt.shortmess:append "c"                    -- Don't give "|ins-completion-menu|" messages.
-
 -- A lua for-loop to loop through the above table of options.
 -- A table is a list, that is allowed key, value pairs.
-for k, v in pairs(options) do
-  vim.opt[k] = v
-end
+  for k, v in pairs(options) do
+    vim.opt[k] = v
+  end
+
+--###########################################################
+--# The line below will update:                             #
+--# The tab character to                 → unicode u2192    #
+--# The end of line character to         ↲ unicode u21b2    #
+--###########################################################
+  -- vim.wo.listchars = { space = '·',eol = '↲',tab = '→',trail= '~',extends = '>',precedes = '<' }
+  vim.o.list = true                        -- Set special formatting characters to on.
+  -- vim.o.listchars = 'eol:↲,tab:→,trail:~,precedes:<,extends:>,space:·'
+  vim.o.listchars = 'eol:↲,trail:~,precedes:<,extends:>,space:·'
 
 -- vim.cmd Passes strings, that will be interpreted as vim-script.
 -- vim.cmd "set whichwrap+=<,>,[,],h,l" Want to find out what it does, before blindly enabling it.
-vim.cmd [[set iskeyword+=-]] -- Will treat words with a dash as part of a single word.
+  vim.cmd [[set iskeyword+=-]] -- Will treat words with a dash as part of a single word.
 --vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work

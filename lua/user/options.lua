29dbc888b526d3fb3100dc4a7952b1666e2b2acf
Kevin James Lausen <opensourceconstruction@gmail.com>
2022-05-31 17:48:41 -0400

    Refactored options.lua into categories for readability.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,39 +1,44 @@
 -- :help options
 local options = {
-  backup = true,                                       -- Creates a backup file, before edits are written.
-  clipboard = "unnamedplus",                           -- Allows neovim to access the system clipboard.
-  cmdheight = 2,                                       -- More space in the neovim command line, for displaying messages.
-  completeopt = { "menuone", "noselect" },             -- Mostly just for cmp.
-  conceallevel = 0,                                    -- Added so `` is visible in markdown files.
+-- Display settings:
   fileencoding = "utf-8",                              -- The encoding written to a file.
-  hlsearch = true,                                     -- Highlight all matches on when searching.
-  incsearch = true,                                    -- Turn on incremental search highlighting.
-  ignorecase = true,                                   -- Ignore case in search patterns.
-  mouse = "a",                                         -- Allow the mouse to be used in neovim.
-  pumheight = 10,                                      -- Pop up menu height.
   showtabline = 2,                                     -- Always show tabs.
-  smartcase = false,                                   -- Overides ignore case, w/ capitals. Default = false.
-  smartindent = true,                                  -- Keep indention from line-to-line.
+  cmdheight = 2,                                       -- More space in the neovim command line, for displaying messages.
+  pumheight = 10,                                      -- Pop up menu height.
   splitbelow = true,                                   -- Force all horizontal splits, to go below current window.
   splitright = true,                                   -- Force all vertical splits, to go to the right of current window.
-  swapfile = false,                                    -- Creates a swapfile.
+-- Theme:
   termguicolors = true,                                -- Set terminal GUI colors.
+  cursorline = true,                                   -- Highlight the current line.
+  cursorcolumn = true,                                 -- Enable a vertical column.
+  number = true,                                       -- Set numbered lines.
+  relativenumber = true,                               -- Set relative numbered lines.
+  numberwidth = 4,                                     -- set number line column width up to 4(default 4).
+  signcolumn = "yes",                                  -- Always show the sign column, or it would shift the text each time.
+-- Functionality:
+  mouse = "a",                                         -- Allow the mouse to be used in neovim.
+  backup = true,                                       -- Creates a backup file, before edits are written.
+  clipboard = "unnamedplus",                           -- Allows neovim to access the system clipboard.
+  swapfile = true,                                     -- Creates a swapfile.
   timeoutlen = 1000,                                   -- Time to wait, for a mapped sequence to complete(in milliseconds).
   undofile = true,                                     -- Enable persistent undo.
   undodir = "~/.config/nvim/persistant_undo_history",  -- Sets the directory to store persistant-undo saves.
   updatetime = 300,                                    -- Faster completion(4000ms default).
   writebackup = true,                                  -- Write a backup file, before saving.
+  completeopt = { "menuone", "noselect" },             -- Mostly just for cmp. ???
+-- Text-formatting:
+  conceallevel = 0,                                    -- Added so `` is visible in markdown files.
+  smartindent = true,                                  -- Keep indention from line-to-line.
   expandtab = true,                                    -- Convert tabs into spaces.
   shiftwidth = 4,                                      -- The number of spaces inserted for each indentation
   tabstop = 4,                                         -- Insert 2 spaces for each tab.
-  cursorline = true,                                   -- Highlight the current line.
-  cursorcolumn = true,                                 -- Enable a vertical column, that makes a code cross-hair on the current position.
-  number = true,                                       -- Set numbered lines.
-  relativenumber = true,                               -- Set relative numbered lines.
-  numberwidth = 4,                                     -- set number line column width to 4(default 4).
-  signcolumn = "yes",                                  -- Always show the sign column, or it would shift the text each time.
   wrap = true,                                         -- Wrap my code-lines on the visible screen.
   guifont = "monospace:h17",                           -- The font used in graphical neovim applications.
+-- Search:
+  hlsearch = true,                                     -- Highlight all matches on when searching.
+  incsearch = true,                                    -- Turn on incremental search highlighting.
+  ignorecase = true,                                   -- Ignore case in search patterns.
+  smartcase = false,                                   -- Overides ignore case, w/ capitals. Default = false.
 }
 
 -- A lua for-loop to loop through the above table of options.
@@ -42,18 +47,12 @@ local options = {
     vim.opt[k] = v
   end
 
--- Did NOT fit well within the options loop above, without throwing errors and disabling the loop.
-  vim.opt.shortmess:append "I"                    -- Set No Intro-message for Neo-vim.
-
---###########################################################
---# The line below will update:                             #
---# The tab character to                 → unicode u2192    #
---# The end of line character to         ↲ unicode u21b2    #
---###########################################################
-  -- vim.wo.listchars = { space = '·',eol = '↲',tab = '→',trail= '~',extends = '>',precedes = '<' }
-  vim.o.list = true                        -- Set special formatting characters to on.
-  -- vim.o.listchars = 'eol:↲,tab:→,trail:~,precedes:<,extends:>,space:·'
+-- Enabling editor pips/EOL/trails/ext/pre...
+  vim.o.list = true  -- Set special formatting characters to on.
   vim.o.listchars = 'eol:↲,trail:~,precedes:<,extends:>,space:·'
+
+-- Did NOT fit well within the options loop , without throwing errors and disabling the loop.
+  vim.opt.shortmess:append "I"                    -- Set No Intro-message for Neo-vim.
 
 -- vim.cmd Passes strings, that will be interpreted as vim-script.
 -- vim.cmd "set whichwrap+=<,>,[,],h,l" Want to find out what it does, before blindly enabling it.

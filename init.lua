ce8b9372df1ed014b992562e77f8393e5e792f3c
Kevin James Lausen <opensourceconstruction@gmail.com>
2022-06-01 19:10:05 -0400

    Added a ton of text substitutions, mostly for auto-correction.

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
+-- Below is the sourced file where I keep most of my general options.
+-- Please remember the lua @ the begining & end of the string are implied.
+-- In other words "user.abbrs" is the same as "lua.user.abbrs" | "user.abbrs.lua" | "lua.user.abbrs.lua".
 require "user.options"
+-- I have auto-commands for cursor-file persistance,
+-- auto. vertically centering, & auto-saving text-folds.
 require "user.autocmds"
+-- Abbreviations for manual text-replacements.
+-- Populated mostly with a top 100 list of commonly misspelled words.
+require "user.abbrs"

c12334a630135ebb3a10533ee82ccbc4164716fc
Briain O'Driscoll <briainodriscoll@gmail.com>
2022-06-01 01:12:43 +0100

    increase timeoutlen

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -40,8 +40,9 @@ keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
 -- Insert --
--- Press jk fast to enter
+-- Press jk or kj quickly to escape to normal mode
 keymap("i", "jk", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
 
 -- Visual --
 -- Stay in indent mode

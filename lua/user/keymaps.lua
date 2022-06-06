8fdf462e12bf6ad977334d3326ae513055febaec
Kevin James Lausen <opensourceconstruction@gmail.com>
2022-06-05 21:16:29 -0400

    Added my options, & keymaps from Lvl-1-2, and an abbrs file for spelling/info.expansion.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -68,3 +68,4 @@ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -44,3 +44,4 @@ end
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+

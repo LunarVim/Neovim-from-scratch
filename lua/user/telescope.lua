03c342602c630341d0c9b4e174ad68a2ab83fc3f
Frederic Dumont <fdumontmd@gmail.com>
2022-07-07 15:44:19 +0900

    telescope: set planets.show_pluto to true

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -85,6 +85,9 @@ telescope.setup {
     -- }
     -- Now the picker_config_key will be applied every time you call this
     -- builtin picker
+    planets = {
+      show_pluto = true,
+    },
   },
   extensions = {
     -- Your extension configuration goes here:

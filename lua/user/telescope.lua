30724c900dd63174f805b56ec4bbd0275c3ccef0
Vũ Thiện Ân <vuthienan178@MacBook-Air-cua-Vu.local>
2022-08-07 11:59:53 +0700

    ignore file larger than 100mb in telescope find files

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -4,13 +4,28 @@ if not status_ok then
 end
 
 local actions = require "telescope.actions"
+local previewers = require("telescope.previewers")
+-- disable file larger than 100mbs
+local new_maker = function(filepath, bufnr, opts)
+  opts = opts or {}
 
+  filepath = vim.fn.expand(filepath)
+  vim.loop.fs_stat(filepath, function(_, stat)
+    if not stat then return end
+    if stat.size > 100000 then
+      return
+    else
+      previewers.buffer_previewer_maker(filepath, bufnr, opts)
+    end
+  end)
+end
 telescope.setup {
   defaults = {
 
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
+    buffer_previewer_maker = new_maker,
 
     mappings = {
       i = {

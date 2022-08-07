76e1d0029477638e24598eb9c937108b0c713ec5
Vũ Thiện Ân <vuthienan178@MacBook-Air-cua-Vu.local>
2022-08-07 11:31:43 +0700

    update treesitter: prevent auto install, install only require module

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,12 +4,13 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
+	ensure_installed = {"lua", "c_sharp", "bash", "java", "json" , "sql", "comment"}, -- one of "all" or a list of languages
 	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
 	},
+  auto_install = false,
 	autopairs = {
 		enable = true,
 	},

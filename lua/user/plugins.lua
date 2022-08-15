7a7dc003bfabc2202afee26dc67ee81414d50417
David Giacomo <dgiacomo@mux.com>
2022-08-15 13:44:16 -0700

    pinning darkplus

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -70,7 +70,7 @@ return packer.startup(function(use)
 
 	-- Colorschemes
 	use({ "folke/tokyonight.nvim", commit = "8223c970677e4d88c9b6b6d81bda23daf11062bb" })
-	use("lunarvim/darkplus.nvim")
+	use({ "lunarvim/darkplus.nvim", commit = "bd8ab5fc71a3c85c41231b187a2add4a78129c4d" })
 
 	-- cmp plugins
 	use({ "hrsh7th/nvim-cmp", commit = "df6734aa018d6feb4d76ba6bda94b1aeac2b378a" }) -- The completion plugin

5559d801b96b06fd4bed70429324156adf2ac3dc
Matej Straka <strakammm@gmail.com>
2022-08-15 07:43:22 +0200

    Update treesitter.lua (#210)
    Hey Chris,
    I copied your treesitter.lua config and it crashed because of ensure_installed option was set to "maintained".
    It looks like it was already deprecated and so I suggest to replace it by ensure_installed="all".

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -14,7 +14,7 @@
 
 local configs = require("nvim-treesitter.configs")
 configs.setup {
-  ensure_installed = "maintained",
+  ensure_installed = "all",
   sync_install = false, 
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {

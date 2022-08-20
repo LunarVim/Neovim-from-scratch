8a35483fef69e91e9d6581288b77ce4413a83ea8
Shirshendu Saha <87955430+sahashirshendu@users.noreply.github.com>
2022-08-20 11:04:00 +0530

    Update to NVim-TS-Context-Commentstring Integration
    Update to NVim-TS-Context-Commentstring Integration [`37a97a0`](https://github.com/JoosepAlviste/nvim-ts-context-commentstring/commit/37a97a04c39f26fffe7745815517e1ce1a0eb3be)

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -4,19 +4,6 @@ if not status_ok then
 end
 
 comment.setup {
-  pre_hook = function(ctx)
-    local U = require "Comment.utils"
-
-    local location = nil
-    if ctx.ctype == U.ctype.block then
-      location = require("ts_context_commentstring.utils").get_cursor_location()
-    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-      location = require("ts_context_commentstring.utils").get_visual_start_location()
-    end
-
-    return require("ts_context_commentstring.internal").calculate_commentstring {
-      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-      location = location,
-    }
-  end,
+  ignore = "^$",
+  pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 }

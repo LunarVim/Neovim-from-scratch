14f28d158a36632cdfbd9bebad9e76cdcaf04c90
David Giacomo <dgiacomo@mux.com>
2022-08-22 22:00:43 -0700

    enable virtual text dap - show var values in grey

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -8,4 +8,5 @@ require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
 require('dap-go').setup()
 require('go').setup()
+require("nvim-dap-virtual-text").setup()
 require('telescope').load_extension('dap')

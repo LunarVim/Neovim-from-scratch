b096a0d26d6bb2fe7cf08191b4d9063113beb8af
mk1121 <maruf.3666@gmail.com>
2022-08-25 02:52:56 +0600

    add tailwindcss lsp

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua","tsserver" }
+local servers = { "jsonls", "sumneko_lua","tsserver","tailwindcss" }
 
 lsp_installer.setup({
 	ensure_installed = servers,

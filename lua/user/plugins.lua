9ea1fe001d2f91444079398c7347ff2f39f9f188
Abheet Chaudhary <abheetchaudhary25@gmail.com>
2022-10-16 06:02:48 +0530

    All set from LSP video except document_highlight.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -64,7 +64,7 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-nvim-lsp"  -- its an extra source for cmp. Its the [LSP] one
   use "hrsh7th/cmp-nvim-lua"
 
   -- snippets

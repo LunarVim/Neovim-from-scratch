304da8496a4eafc3eac6bd863fa337ca761e37a4
Briain O'Driscoll <briainodriscoll@gmail.com>
2022-11-16 17:11:39 +0000

    chore(whichkey): update quickfix command

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -151,7 +151,7 @@ local mappings = {
       "Prev Diagnostic",
     },
     l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-    q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", "Quickfix" },
+    q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
     r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
     s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
     S = {

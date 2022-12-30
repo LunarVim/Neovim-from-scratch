0e2861d684c26dffa74b356ac92421762b04cdd8
Everton Jr <evertonse.junior@gmail.com>
2022-12-30 18:07:36 -0300

    key maps again

--- lua/cybervim/keymaps.lua
+++ lua/cybervim/keymaps.lua
@@ -1,9 +1,15 @@
 local opts = { noremap = true, silent = true }
-
 local term_opts = { silent = true }
+
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
+
+-- Straight Map, more general, can map key to a func, can accept more than one mode 
+-- ex: map({'n','i'},<space>pr, function() print('hello') end, opts)
+-- This means that <space>pr will run the function that print hello,
+-- both insert mode and normal mode 
+local map = vim.keymap.set
 
 -- [[ Basic Keymaps ]]
 -- See `:help mapleader`
@@ -84,136 +90,150 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 
 --NvimTree @Conflict probably
-keymap("n", "<leader>o", ":NvimTreeOpen<CR>"            ,opts)
-keymap("n", "<leader>e", ":NvimTreeToggle<CR>"          ,opts)
-keymap("n", "<leader>r", ":NvimTreeRefresh<CR>"         ,opts)
-keymap("n", "<leader>n", ":NvimTreeFindFileToggle<CR>"  ,opts)
-keymap("n", "<leader>f", ":NvimTreeFocus<CR>"           ,opts)
+keymap("n", "<leader>to", ":NvimTreeOpen<CR>"            ,opts)
+keymap("n", "<leader>tt", ":NvimTreeToggle<CR>"          ,opts)
+keymap("n", "<leader>tr", ":NvimTreeRefresh<CR>"         ,opts)
+keymap("n", "<leader>tn", ":NvimTreeFindFileToggle<CR>"  ,opts)
+keymap("n", "<leader>tf", ":NvimTreeFocus<CR>"           ,opts)
 
 
 -- >> move fast with crtl move
-vim.keymap.set("n", '<C-h>', "b",    {noremap = true, silent = true}) 
-vim.keymap.set("n", '<C-l>', "w",    {noremap = true, silent = true}) 
+keymap("n", '<C-h>', "b",    opts) 
 
-vim.keymap.set("n", '<C-Left>', "b",{noremap = true, silent = true})  
-vim.keymap.set("n", '<C-Right>',  "w",{noremap = true, silent = true})  
+keymap("n", '<C-l>', "w",    opts) 
+keymap("n", '<C-Left>', "b",opts)  
+keymap("n", '<C-Right>',  "w",opts)  
 
-vim.keymap.set('n', '<C-j>', "}",{noremap = true, silent = true})
-vim.keymap.set('n', '<C-k>', "{",{noremap = true, silent = true})
+keymap('n', '<C-j>', "}",opts)
+keymap('n', '<C-k>', "{",opts)
 
-vim.keymap.set('n', '<C-Up>', "{",{noremap = true, silent = true})
-vim.keymap.set('n', '<C-Down>', "}",{noremap = true, silent = true})
+keymap('n', '<C-Up>', "{",opts)
+keymap('n', '<C-Down>', "}",opts)
 
 
-vim.keymap.set("i", '<C-Up>', 
+map("i", '<C-Up>', 
   function ()
     print("hello")
     vim.call("spellbadword")
   end, 
-
-  {noremap = true, silent = true}
+  opts
 )
 -- << move fast with crtl move
 
 -- >> Shift Selection :
-vim.keymap.set('n', '<S-Up>', "v<Up>",{noremap = true, silent = true})
-vim.keymap.set('n', '<S-Down>', "v<Down>",{noremap = true, silent = true})
+keymap('n', '<S-Up>', "v<Up>",opts)
+keymap('n', '<S-Down>', "v<Down>",opts)
 
-vim.keymap.set('n', '<S-Left>', "v",{noremap = true, silent = true})
-vim.keymap.set('n', '<S-Right>', "v",{noremap = true, silent = true})
+keymap('n', '<S-Left>', "<Left>v",opts)
+keymap('n', '<S-Right>', "v",opts)
 
-vim.keymap.set('i', '<S-Up>', "<C-o>v<Up>",{noremap = true, silent = true})
-vim.keymap.set('i', '<S-Down>', "<C-o>v<Down>",{noremap = true, silent = true})
+keymap('i', '<S-Up>', "<C-o>v<Up>",opts)
+keymap('i', '<S-Down>', "<C-o>v<Down>",opts)
 
-vim.keymap.set('i', '<S-Left>', "<C-o>v<Left>",{noremap = true, silent = true})
-vim.keymap.set('i', '<S-Right>', "<C-o>v<Right>",{noremap = true, silent = true})
+keymap('i', '<S-Left>', "<Left><C-o>v",opts)
+keymap('i', '<S-Right>', "<C-o>v",opts)
 
-vim.keymap.set('v', 'i', "<ESC><ESC>",{noremap = true, silent = true})
-vim.keymap.set('v', '<S-Up>', "<Up>",{noremap = true, silent = true})
-vim.keymap.set('v', '<S-Down>', "<Down>",{noremap = true, silent = true})
+keymap('v', 'i', "<ESC><ESC>",opts)
+keymap('v', '<S-Up>', "<Up>",opts)
+keymap('v', '<S-Down>', "<Down>",opts)
 
-vim.keymap.set('v', '<S-Left>', "<Left>",{noremap = true, silent = true})
-vim.keymap.set('v', '<S-Right>', "<Right>",{noremap = true, silent = true})
+keymap('v', '<S-Left>', "<Left>",opts)
+keymap('v', '<S-Right>', "<Right>",opts)
 -- << Shift Selection :
 
 -- >> Set back and forth in code to be Alt keys
-vim.keymap.set('n', '<M-Left>', "<C-o>",{noremap = true, silent = true})
-vim.keymap.set('n', '<M-Right>', "<C-i>",{noremap = true, silent = true})
+keymap('n', '<M-Left>', "<C-o>",opts)
+keymap('n', '<M-Right>', "<C-i>",opts)
 -- << Shift Selection :
 -- <<
 
 -- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+-- See `:help keymap()`
+map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
-vim.keymap.set("n", "<leader><leader>e", ":NvimTreeToggle<CR>")
-vim.keymap.set("n", "<C-s>", ":/")
-
-vim.keymap.set("n", '<M-Up>', "ddkP") --// Moving the line up
-vim.keymap.set("n", '<M-Down>', "ddjP") -- // Moving the line down
+keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 
-vim.keymap.set("i", '<C-s>', "<C-c>:w<CR>li") -- saving with crt + s on insert mode 
-vim.keymap.set("n", '<C-s>', ":w<CR>") -- saving with crt + s on insert mode 
 
-vim.keymap.set("v", '<M-Up>', ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", '<M-Down', ":m '>+1<CR>gv=gv")
+keymap("n", '<M-Up>', "ddkP",opts) --// Moving the line up
+keymap("n", '<M-Down>', "ddjP",opts) -- // Moving the line down
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+
+keymap("i", '<C-s>', "<C-o>:w<CR>",opts) -- saving with crt + s on insert mode 
+keymap("n", '<C-s>', ":w<CR>", opts) -- saving with crt + s on insert mode 
+
+keymap("v", '<M-Up>', ":m '<-2<CR>gv=gv",opts)
+keymap("v", '<M-Down', ":m '>+2<CR>gv=gv",opts)
+
+keymap("n", "J", "mzJ`z",opts)
+keymap("n", "<C-d>", "<C-d,optss>zz",opts)
+keymap("n", "<C-u>", "<C-u,optss>zz",opts)
+keymap("n", "n", "nzzzv" ,opts)
+keymap("n", "N", "Nzzzv" ,opts)
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", "\"_dP")
+keymap("x", "<leader>p", "\"_dP",opts)
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+keymap("n", "<leader>y", "\"+y",opts)
+keymap("v", "<leader>y", "\"+y",opts)
+keymap("n", "<leader>Y", "\"+Y",opts)
 
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+keymap("n", "<leader>d", "\"_d",opts)
+keymap("v", "<leader>d", "\"_d",opts)
 
-vim.keymap.set("n", "<leader>p", "\"+p")
-vim.keymap.set("v", "<leader>p", "\"+p")
-vim.keymap.set("n", "<leader>p", "\"+P")
+keymap("n", "<leader>p", "\"+p",opts)
+keymap("v", "<leader>p", "\"+p",opts)
+keymap("n", "<leader>p", "\"+P",opts)
 
 -- Maybe the same button for entering in both modes ??
-vim.keymap.set("i", "<C-c>", "<Esc>")
+keymap("i", "<C-c>", "<Esc>",opts)
+keymap("x", "<C-c>", "<Esc>",opts)
 
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", function()
+keymap("n", "Q", "<nop>",opts)
+--Dont know what this does--keymap("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+map("n", "<leader>for", function()
   vim.lsp.buf.format()
 end)
 
---vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
---vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
-vim.keymap.set("n", "<leader>r", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+--keymap("n", "<C-k>", "<cmd>cnext<CR>zz")
+--keymap("n", "<C-j>", "<cmd>cprev<CR>zz")
+--keymap("n", "<leader>k", "<cmd>lnext<CR>zz")
+--keymap("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+map('n', '[d', vim.diagnostic.goto_prev,opts)
+map('n', ']d', vim.diagnostic.goto_next,opts)
 
--- Shift Tab undident line 
+--[O]pen [F]loat
+map('n', '<leader>of', vim.diagnostic.open_float, opts)
+--[L][O]clist
+map('n', '<leader>lo', vim.diagnostic.setloclist, opts)
+
+-- Shift Uab undident line 
 keymap('i', '<S-Tab>', "<C-d>",opts)
-keymap('i', '<C-BS>','<C-w>',opts) 
+keymap('i', '<C-BS>','<C-o>db',opts) 
 
 -- <leader>te [T]oggleT[E]rm
 keymap('n', '<leader>te',':ToggleTerm<CR>',opts)
 keymap('t', '<leader>te','<ESC><ESC>:ToggleTerm<CR>',opts)
 -- make it work on Terminal mode
 
-keymap('n', '<leader>ds',':Alpha<CR>',opts)
+-- Undo with C-u on insert mode 
+keymap('i', '<C-u>','<C-o>u',opts) 
+-- Undo with C-u on insert mode 
+keymap('i', '<C-r>','<C-o><C-r>',opts) 
+
+-- C-a already does ths 
+--keymap('n', '<leader>ds',':Alpha<CR>',opts)
+keymap('i', '<BS>','<ESC>:lua print("hello")',opts)
+
+--[R][E]place shortcut 
+keymap('v', '<leader>re', 'y:%s/<C-r>*/<C-r>*/<Left><Down>', opts)
+keymap('n', '<leader>re', 'yW:%s/<C-r>*/<C-r>*/<Left><Down>', opts)
+--[S][E]arch shortcut 
+keymap("n", "<leader>ee", ":/",opts)
+
